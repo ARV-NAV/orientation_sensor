89990d69efb4e79091971f5082845778585af906
Gregory Sanchez <sanchez.gregory886@gmail.com>
2020-02-26 18:33:10 -0800

    Add working python IMU parser

--- parse_imu.py
+++ parse_imu.py
@@ -1,23 +1,47 @@
-import datetime
-import os
-import matlab.engine as mEngine
-import array
-import pprint
+"""Python wrapper to parse the IMU binary data
 
-pp = pprint.PrettyPrinter(indent=2)
-t1 = datetime.datetime.now()
-eng = mEngine.start_matlab()
-t2 = datetime.datetime.now()
-delta = t2-t1
-print("Engine build time (ms):", delta.total_seconds() * 1000) 
+    Module to read binary data outputted from the IMU and convert it to a usable python object
+    Uses a predefined MatLab parser to parse through the binary data.
+    The parsed data is converted into a dict containing all of the IMU data.
+"""
 
+# ================ Built-in Imports ================ #
 
+from time import time
+
+# ================ Third Party Imports ================ #
+
+import matlab.engine as m_engine
+
+# ================ Authorship ================ #
+
+__author__ = "Chris Patenaude"
+__contributors__ = ["Chris Patenaude", "Gabriel Michael", "Gregory Sanchez"]
+
+# ================ Global Variables ================ #
+
+eng = m_engine.start_matlab()
 filename = "sample_raw_data.bin"
 
-fileContent = None
-with open(filename, mode='rb') as file:
-    file.seek(-1000, os.SEEK_END)
-    fileContent = file.read();
 
-data = eng.parse_imu(fileContent)
-print(data)
+def get_last_orientation() -> dict:
+    """Get Last Orientation
+        Grabs the last set of binary values from the binary file.
+        Opens and reads the file through MatLab itself to then send it through the parser
+
+    @return: dict: data containing the orientations
+    """
+    fp = eng.matlab.io.datastore.DsFileReader(filename)
+    eng.seek(fp, -1000, 'Origin', 'end-of-file')  # This is supposed to set the file pointer to the end of the file. it works on matlab, but not here
+    ml_bin_arr = eng.read(fp, 1000)
+
+    start_time = time()
+    orientation = eng.parse_imu(ml_bin_arr)
+    print("Parse time {0}s".format(time() - start_time))
+
+    return orientation
+
+
+if __name__ == "__main__":
+    data = get_last_orientation()
+    print(data)
