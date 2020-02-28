6dd3fcf7a26636bafd1776633929299c5fa79528
Michael Gabriel <michaelngabriel@gmail.com>
2020-02-28 15:51:21 -0800

    added docstring for orientation dictionary

--- parse_imu.py
+++ parse_imu.py
@@ -36,15 +36,36 @@ def get_last_orientation():
     # print(eng.seek(fp, 100, 'RespectTextEncoding', True, 'end-of-file', True))
 
     start_time = time()
+     """Keys of the orientation ditc
+     'units': { 'attitude': { 'compensated_angular_rate': { 'X': 'rads/sec',
+                                                         'Y': 'rads/sec',
+                                                         'Z': 'rads/sec',
+                                                         'valid': '1=valid, 0=invalid'},
+                           'gps_timestamp': { 'time_of_week': 'seconds',
+                                              'valid': '1=valid, 0=invalid',
+                                              'week_number': 'n/a'},
+                           'heading_update_source_state': { 'heading': 'radians',
+                                                            'heading_1_sigma_uncertainty': 'radians',
+                                                            'source': '0=no source, 1=Magnetometer, 4=External',
+                                                            'valid': '1=valid, 0=invalid'},
+                           'linear_acceleration': { 'X': 'm/sec^2',
+                                                    'Y': 'm/sec^2',
+                                                    'Z': 'm/sec^2',
+                                                    'valid': '1=valid, 0=invalid'},
+                           'orientation_euler_angles': { 'pitch': 'radians',
+                                                         'roll': 'radians',
+                                                         'valid': '1=valid, 0=invalid',
+                                                         'yaw': 'radians'}}}}
+     """
     orientation = eng.parse_imu(filename)
     print("Parse time {0}s".format(time() - start_time))
 
-    test_dict = {'heading':np.asarray(oreintaion['attitude']['heading_update_source_state']['heading']),
-                 'pitch':np.asarray(oreintaion['attitude']['orientation_euler_angles']['pitch']),
-                 'roll':np.asarray(oreintaion['attitude']['orientation_euler_angles']['roll']),
-                 'yaw':np.asarray(oreintaion['attitude']['orientation_euler_angles']['yaw']),
-                 'valid_heading':np.asarray(oreintation['attitude']['orientation_euler_angles']['valid']),
-                 'valid_oreintation':np.asarray(oreintation['attitude']['orientation_euler_angles']['valid'])
+    test_dict = {'heading':np.asarray(orientation['attitude']['heading_update_source_state']['heading']),
+                 'pitch':np.asarray(orientation['attitude']['orientation_euler_angles']['pitch']),
+                 'roll':np.asarray(orientation['attitude']['orientation_euler_angles']['roll']),
+                 'yaw':np.asarray(orientation['attitude']['orientation_euler_angles']['yaw']),
+                 'valid_heading':np.asarray(orientation['attitude']['orientation_euler_angles']['valid']),
+                 'valid_orientation':np.asarray(orientation['attitude']['orientation_euler_angles']['valid'])
     }
     return test_dict
 
@@ -52,4 +73,3 @@ def get_last_orientation():
 if __name__ == "__main__":
     data = get_last_orientation()
     pp.pprint(data)
-
